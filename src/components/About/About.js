f5c8bdc6d13e2b4e2dda1396d33a03e491878d55
Tom O'Donovan <tom.odonovan01@gmail.com>
2023-03-28 18:56:32 +1100

    Create basic pages allowing navigation between each

---
+++ src/components/About/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>I am a Front-End developer based in Sydney, Australia.</p>
+        </div>
+    )
+}
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+        <h1>Contact</h1>
+        <ul>
+            <li>Phone:</li>
+            <li>Email:</li>
+            <li>LinkedIn:</li>
+            <li>Github:</li>
+        </ul>
+    </div>
+  )
+}
